a611dba5aaf44f30e513a86c925332027bec22f5
Vitaly Haritonsky <vharitonsky@gmail.com>
2013-11-09 13:48:58 +0200

    added riples

--- server.py
+++ server.py
@@ -28,6 +28,11 @@ def sse():
 def letters():
     return open('letters.html').read()
 
+@route('/ripples')
+def ripples():
+    return open('ripples.html').read()
+
+
 @route('/static/<path:path>')
 def callback(path):
     return static_file(path, 'static')
